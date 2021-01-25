cd2fb05e634ea29073133c153f322918acb973dd
DubovtsevEgor <egor.dubovczcev@yandex.ru>
2021-01-25 14:50:28 +0400

    added execution service

--- src/main/java/org/bank/service/ExecutionService.java
+++ src/main/java/org/bank/service/ExecutionService.java
@@ -9,16 +9,20 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class ExecutionService {
     private static final int NUMBER_OF_THREAD = 20;
     private static final int NUMBER_OF_OPERATIONS_IN_EACH_THREAD = 50;
     private static final int TOTAL_COUNT_OF_ACCOUNTS = 10;
     private static final Path PATH_TO_ACCOUNTS_DIR = Paths.get("src/main/resources/accounts");
-    private final List<UserThread> threads = new ArrayList<>();
     private static final AccountDao ACCOUNT_DAO = new AccountDao(PATH_TO_ACCOUNTS_DIR);
     private final AccountService accountService = new SimpleAccountService(ACCOUNT_DAO);
     private final AccountsManagerService accountsManagerService = new SimpleAccountsManagerService(ACCOUNT_DAO);
+    private final ExecutorService executorService = Executors.newFixedThreadPool(NUMBER_OF_THREAD);
+
 
     public void startThreads() {
         List<Account> accounts = new ArrayList<>();
@@ -30,32 +34,24 @@ public class ExecutionService {
             e.printStackTrace();
         }
 
+
         for (int i = 0; i < NUMBER_OF_THREAD; i++) {
-            UserThread userThread = new UserThread("user thread " + i,
-                    accountService, accounts);
-            threads.add(userThread);
+            UserThread userThread = new UserThread("user thread " + i, accountService, accounts);
             userThread.setOperationsInEachThread(NUMBER_OF_OPERATIONS_IN_EACH_THREAD);
-            userThread.start();
+            executorService.submit(userThread);
         }
 
-        while (Thread.currentThread().isAlive()) {
-            try {
-                if (threads.stream().noneMatch(Thread::isAlive)) {
+        try {
+            executorService.shutdown();
+            executorService.awaitTermination(1000, TimeUnit.MILLISECONDS);
+            accountsManagerService.saveAllAccounts(accounts);
+            accountsManagerService.getAllAccounts();
 
-                    accountsManagerService.saveAllAccounts(accounts);
-                    accountsManagerService.getAllAccounts();
-
-                    break;
-                }
-
-                Thread.sleep(500);
-
-            } catch (AccountDeserializeException e) {
-                e.printStackTrace();
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                e.printStackTrace();
-            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            e.printStackTrace();
+        } catch (AccountDeserializeException e) {
+            e.printStackTrace();
         }
     }
 
